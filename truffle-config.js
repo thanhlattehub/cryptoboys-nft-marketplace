b1d02008ef52c6069f38db423ccb9037e8d2d75e
thanhlattehub <79505800+thanhlattehub@users.noreply.github.com>
2022-03-17 11:07:47 +0700

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -13,7 +13,7 @@ module.exports = {
   contracts_build_directory: "./src/abis/",
   compilers: {
     solc: {
-      version: "pragma",
+      version: "0.7.6",
       optimizer: {
         enabled: true,
         runs: 200,
